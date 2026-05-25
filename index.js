9e19db340167aa6918ecb5a391c1866574b4b90f
Arunkumar1321 <akmirror619@gmail.com>
2026-05-25 20:46:50 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const express=require("express");
+const cors=require("cors");
+const app=express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
+app.listen(5000,()=>{console.log("Server is running.....")})
+
+var users=[{email:"1234@gmail.com", phone:"1234567890", password:"123"}]
+app.post("/",(req,res)=>{
+    const {user,password}=req.body
+    const existingUser=users.some(u => (u.email === user || u.phone === user) && u.password === password);
+    if(existingUser){
+        res.send(true)
+    } else {
+        res.send(false)
+    }
+})
+
+app.post("/sign",(req,res)=>{
+    const {user,password}=req.body
+    const alreadyexists = users.some(u => u.email === user || u.phone === user);
+    if(alreadyexists){
+        res.send(false)
+    }else{
+    users.push({email:user, phone:user, password})
+    res.send(true)
+    }
+    console.log("Current users:", users);
+})
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
