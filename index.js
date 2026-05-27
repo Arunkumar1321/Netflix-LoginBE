8b9ecbb279df15bf15d8bce5c1e86c6168aa11f6
Arunkumar1321 <akmirror619@gmail.com>
2026-05-27 19:13:53 +0530

    Mongo DB integration is done

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
 const express=require("express");
 const cors=require("cors");
+const mongoose=require("mongoose");
 const app=express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
+// DB connected to backend server
+mongoose.connect("mongodb://127.0.0.1:27017/netflix-login").then(()=>{console.log("DB attached sucessfully")})
+.catch(()=>{console.log("DB failed to connect")})
+//model attachment for collections
+const users = mongoose.model("user",{email:String,phone:String,password:String},"user")
+users.find().then((retdata)=>{
+    console.log(retdata)
+})
+
 app.listen(5000,()=>{console.log("Server is running.....")})
 app.get("/",(req,res)=>{
     res.send("Backend is working")
 });
 
-var users=[{email:"1234@gmail.com", phone:"1234567890", password:"123"}]
-app.post("/",(req,res)=>{
+
+app.post("/",async (req,res)=>{
     const {user,password}=req.body
-    const existingUser=users.some(u => (u.email === user || u.phone === user) && u.password === password);
+    const existingUser=await users.findOne({$or:[{email : user} ,{ phone : user}], password : password});
     if(existingUser){
         res.send(true)
     } else {
@@ -20,14 +30,15 @@ app.post("/",(req,res)=>{
     }
 })
 
-app.post("/sign",(req,res)=>{
+app.post("/sign",async(req,res)=>{
     const {user,password}=req.body
-    const alreadyexists = users.some(u => u.email === user || u.phone === user);
+    const alreadyexists =await users.findOne({$or:[{email:user},{phone:user}]});
     if(alreadyexists){
         res.send(false)
     }else{
-    users.push({email:user, phone:user, password})
+   await users.create({email:user, phone:user, password})
     res.send(true)
+    console.log("New user created")
     }
     console.log("Current users:", users);
 })
