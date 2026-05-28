1e85f76d704c5904d6ad7cca11b3d22861acc162
Arunkumar1321 <akmirror619@gmail.com>
2026-05-28 16:00:53 +0530

    Server re worked

--- index.js
+++ index.js
@@ -6,13 +6,14 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 
+
+var users = [{email:"123@gmail.com",phone:"1234567890",password:"123"}]
 app.listen(5000,()=>{console.log("Server is running.....")})
 app.get("/",(req,res)=>{
     res.send("Backend is working")
-});
-var users = {email:"123@gmail.com",phone:"1234567890",password:"123"}
 
-app.post("/",async (req,res)=>{
+});
+app.post("/",(req,res)=>{
     const {user,password}=req.body
      const existingUser=users.some(u => (u.email === user || u.phone === user) && u.password === password);
     if(existingUser){
@@ -22,15 +23,15 @@ app.post("/",async (req,res)=>{
     }
 })
 
-app.post("/sign",async(req,res)=>{
+app.post("/sign",(req,res)=>{
     const {user,password}=req.body
-      const alreadyexists = users.some(u => u.email === user || u.phone === user);
+         const alreadyexists = users.some(u => u.email === user || u.phone === user);
     if(alreadyexists){
         res.send(false)
     }else{
  users.push({email:user, phone:user, password})
     res.send(true)
-    console.log("New user created")
+    console.log("New user created",users)
     }
 })
 const PORT = process.env.PORT || 5000;
